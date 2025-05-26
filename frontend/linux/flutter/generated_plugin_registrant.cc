e190fe384f292ecb0421f30c24104ae4253a82ee
Nash Luffman <nashluffman@gmail.com>
2025-05-26 19:37:15 +0100

    add bits

--- frontend/linux/flutter/generated_plugin_registrant.cc
+++ frontend/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dart_duckdb/dart_duckdb_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) dart_duckdb_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DartDuckdbPlugin");
+  dart_duckdb_plugin_register_with_registrar(dart_duckdb_registrar);
 }
--- frontend/linux/flutter/generated_plugins.cmake
+++ frontend/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  dart_duckdb
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
